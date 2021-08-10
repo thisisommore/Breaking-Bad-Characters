7efab8221a26746276ddf654180a3ec53a0e09b4
Om More <51229945+thisisommore@users.noreply.github.com>
2021-08-10 09:15:47 +0530

    clean(StyledCharactor): Remove unused code

--- src/pages/components/StyledCharactor.tsx
+++ src/pages/components/StyledCharactor.tsx
@@ -1,4 +1,4 @@
-import React, { RefObject, useEffect, useRef, useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import Charactor from "../../types/Charactor";
 const Container = styled.div`
@@ -26,40 +26,13 @@ const Container = styled.div`
   }
 `;
 
-type Postion = {
-  x: number;
-  y: number;
-};
-
 type Props = {
   charactor: Charactor;
   [key: string]: any;
 };
 function StyledCharactor(props: Props) {
-  const ref = useRef<HTMLDivElement>(null);
-  const [postion, setPostion] = useState<Postion>();
-  const [style, setStyle] = useState<React.CSSProperties>();
-  useEffect(() => {
-    setPostion({
-      x:
-        (ref.current?.offsetLeft === 0
-          ? 0
-          : ref.current?.offsetLeft ?? 10 - 10) ?? 0,
-      y: ref.current?.offsetTop ?? 0,
-    });
-  }, [setPostion]);
-
-  const expandDiv = () => {
-    setStyle({
-      position: "absolute",
-    });
-  };
   return (
-    <Container
-      {...props}
-      ref={ref}
-      style={{ top: postion?.y, left: postion?.x, ...style }}
-    >
+    <Container {...props}>
       <p className="name">{props.charactor.name}</p>
       <div className="occupations">
         {props.charactor.occupation.map((ele, i) => (
