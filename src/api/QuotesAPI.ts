dcabd96a1bd480c1bf3bd36979bce00edfe181a6
Om More <51229945+thisisommore@users.noreply.github.com>
2021-08-11 08:21:30 +0530

    Quotes: Get quote by author name (#20)
    - Id was used to get specific quote and thus using author name returns all quotes

--- src/api/QuotesAPI.ts
+++ src/api/QuotesAPI.ts
@@ -1,6 +1,10 @@
 import axiosInstance from "../config/axios-config";
 import { Quote } from "../types/Quote";
 
-export default function getQuotes(charId: string) {
-  return axiosInstance.get<Quote[]>(`/quotes/${charId}`);
+export default function getQuote(authorName: string) {
+  return axiosInstance.get<Quote[]>(`/quote`, {
+    params: {
+      author: authorName,
+    },
+  });
 }
--- src/pages/detail/Detail.tsx
+++ src/pages/detail/Detail.tsx
@@ -11,31 +11,34 @@ import StyledCharacterDetail from "./components/StyledCharacterDetail";
 export const Detail = () => {
   const [character, setCharacter] = useState<Chatactor>();
   const [quotes, setQuotes] = useState<Quote[]>();
-  const { error, loading, getCharacter, getQuotes } = useHttp();
+  const { error, loading, getCharacter, getQuote } = useHttp();
   const { id } = useParams<{ id: string }>();
   const { location } = useHistory<{ character: Chatactor }>();
 
   useEffect(() => {
+    let characterName: string;
+
     if (location.state?.character) {
       //Set data from state instead of fetching from api
+      characterName = location.state.character.name;
       setCharacter(location.state.character);
     } else {
       //Set data from api since data is not available in state
       const fetchCharacter = async () => {
         const res = await getCharacter(id);
-
+        characterName = res.data[0].name;
         setCharacter(res.data[0]);
       };
       fetchCharacter();
     }
 
     const fetchQuotes = async () => {
-      const res = await getQuotes(id);
+      const res = await getQuote(characterName);
       setQuotes(res.data);
     };
 
     fetchQuotes();
-  }, [getCharacter, getQuotes, id, location.state, setCharacter, setQuotes]);
+  }, [getCharacter, getQuote, id, location.state, setCharacter, setQuotes]);
   return (
     <Modal>
       {loading && !character && <Spinner />}
