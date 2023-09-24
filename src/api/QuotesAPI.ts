e13fdaba124403d537037e66c5cea4d02302b206
thisisommore <ommore501@gmail.com>
2023-09-24 10:23:16 +0000

    Fix Quotes API call

--- src/api/QuotesAPI.ts
+++ src/api/QuotesAPI.ts
@@ -2,7 +2,7 @@ import axiosInstance from "../config/axios-config";
 
 export type GetQuotesReponse = string[]
 export default function getQuote(authorName: string) {
-  return axiosInstance.get<GetQuotesReponse>(`/quote`, {
+  return axiosInstance.get<GetQuotesReponse>(`/quotes`, {
     params: {
       author: authorName,
     },
